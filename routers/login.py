b58ce7c06989c444dc8b7e1e307673479a1615f1
Абдельнур Алекс Фатех <a.aabdelnur@mail.ru>
2023-10-16 19:47:34 +0300

    fixed privileges difference

--- routers/login.py
+++ routers/login.py
@@ -10,10 +10,13 @@ async def login(message: Message):
     args = message.text.split()
     if len(args) >= 2:
         ident = int(args[1])
-        replacements[message.chat.id] = ident
-        await message.answer(f"Теперь вы {get_user(message.chat.id).name}")
-    else:
-        await message.answer("Введи все нужные параметры. {id}")
+        user = get_user(ident)
+        admin = get_user(message.chat.id)
+        if admin.access_level > user.access_level:
+            replacements[message.chat.id] = ident
+            return await message.answer(f"Теперь вы {get_user(message.chat.id).name}")
+        return await message.answer(f"Привилегии этого пользователя выше.")
+    await message.answer("Введи все нужные параметры. {id}")
 
 
 @dp.message(lambda x: check_access(x, 10, False) and ("/exit" in x.text))
